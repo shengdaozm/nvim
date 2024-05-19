f9b1307b922d898da304d82033b64c96afff734e
shengdao <211471317@qq.com>
2024-05-19 12:39:43 +0800

    add leetcode.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  --[[ { import = "astrocommunity.pack.lua" }, ]]
+  { import = "astrocommunity.pack.cpp" },
   -- import/override with your plugins folder
 }

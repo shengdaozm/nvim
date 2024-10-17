f501f4b1f1563bb9911febe971fb58d4875d5319
shengdao <211471317@qq.com>
2024-10-17 22:56:53 +0800

    add snip lua file

--- lua/plugins/fittencode.lua
+++ lua/plugins/fittencode.lua
@@ -1,3 +1,6 @@
+-- NOTE: a plugin to help you code with AI,
+-- NOTE: yeah!,I haven't enough money to buy coplit.
+
 return {
   "luozhiya/fittencode.nvim",
   config = function() require("fittencode").setup() end,

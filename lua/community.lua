099fa9168ed79a22acde99c722160bc613f1cdcd
shengdao <211471317@qq.com>
2024-11-04 16:32:53 +0800

    change theme to catppuccin

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  {import = "astrocommunity.completion.cmp-cmdline"}
+  { import = "astrocommunity.completion.cmp-cmdline" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }

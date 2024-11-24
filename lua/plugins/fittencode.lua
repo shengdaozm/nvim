fbd4fccbadde86aff6a09926d7c0f0bd8a0a6e8c
shengdaozm <211471317@qq.com>
2024-11-24 10:45:50 +0800

    change the aicode plugin to codeium

--- lua/plugins/fittencode.lua
+++
@@ -1,7 +0,0 @@
--- NOTE: a plugin to help you code with AI,
--- NOTE: yeah!,I haven't enough money to buy coplit.
-
-return {
-  "luozhiya/fittencode.nvim",
-  config = function() require("fittencode").setup() end,
-}

ba8fe82c750bd56868366401c40d42c96f6cbbe9
shengdaozm <211471317@qq.com>
2025-08-01 01:02:25 +0800

    change version to v5

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,7 +9,7 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     vim.g.tex_flavor = "xelatex" -- 指定tex文件编译器
-    vim.g.vimtex_view_enabled = 0 -- 禁用自动预览
+    -- vim.g.vimtex_view_enabled = 0 -- 禁用自动预览
     vim.g.vimtex_view_method = "zathura" -- 指定pdf预览文件
     vim.g.vimtex_compiler_latexmk = { -- 指定自定义编译命令
       callback = 1, -- 编译完成后执行回调函数 (如自动预览)
